b59f88fa3bbee0069b530c311e626425698fc92a
Rickard <rickard.johansson@ecutbildning.se>
2023-01-12 16:52:12 +0100

    Added Class Menu for Information on adressbok.

--- ConsoleApp-address-book/Program.cs
+++ ConsoleApp-address-book/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using ConsoleApp_address_book.Services;
+
+var menu = new Menu();
+menu.FilePath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\content.json"; 
+
+
+while(true)
+{
+    Console.Clear();
+    menu.WelcomeMenu();
+}
---
+++ ConsoleApp-address-book/Services/Menu.cs
@@ -0,0 +1,88 @@
+﻿using ConsoleApp_address_book.Models;
+using Newtonsoft.Json;
+
+namespace ConsoleApp_address_book.Services
+{
+    internal class Menu
+    {
+        private List<IContact> newcontacts = new List<IContact>();
+        private FileService file = new FileService();
+
+        public string FilePath { get; set; } = null!;
+
+
+        public void WelcomeMenu()
+        {
+            Console.WriteLine("Välkommen till Adressboken");
+            Console.WriteLine("1. Skapa en ny kontakt");
+            Console.WriteLine("2. Visa alla kontakter");
+            Console.WriteLine("3. Visa en specifik kontakt");
+            Console.WriteLine("4. Ta bort en specifik kontakt");
+            Console.Write("Välj ett av alternativen ovan: ");
+            var option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1": OptionOne(); break;
+                case "2": OptionTwo(); break;
+                case "3": OptionThree(); break;
+                case "4": OptionFour(); break;
+            }
+
+            file.Save(FilePath, JsonConvert.SerializeObject(new { newcontacts }));
+        }
+
+        private void OptionOne() 
+        { 
+            Console.Clear();
+            Console.WriteLine("Skapa en ny kontakt");
+
+            CreateNewContact newcontact = new CreateNewContact();
+            Console.Write("Ange förnamn: ");
+            newcontact.FirstName = Console.ReadLine() ?? "";
+            Console.Write("Ange Efternamn: ");
+            newcontact.LastName = Console.ReadLine() ?? "";
+            Console.Write("Ange E-post: ");
+            newcontact.Email = Console.ReadLine() ?? "";
+            Console.Write("Ange Telefonnummer: ");
+            newcontact.Phone = Console.ReadLine() ?? "";
+            Console.Write("Ange Adress: ");
+            newcontact.Address = Console.ReadLine() ?? "";
+            Console.Write("Ange Stad: ");
+            newcontact.City = Console.ReadLine() ?? "";
+
+            newcontacts.Add(newcontact);
+        }
+
+
+
+
+
+
+
+
+
+
+        private void OptionTwo()
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+        }
+
+        private void OptionThree()
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+        }
+
+        private void OptionFour()
+        {
+            Console.Clear();
+            Console.WriteLine();
+
+        }
+
+    }
+}
