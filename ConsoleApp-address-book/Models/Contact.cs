963e09d2dfe6c3de9f05870493f8ebe671b497e0
Rickard <rickard.johansson@ecutbildning.se>
2023-02-05 19:03:38 +0100

    Cleaning code

--- ConsoleApp-address-book/Models/Contact.cs
+++ ConsoleApp-address-book/Models/Contact.cs
@@ -9,7 +9,6 @@ public interface IContact
     string LastName { get; set; }
     string Phone { get; set; }
     string DisplayNameEmail => $"\n{"Förnamn: " + FirstName} \n{"Efternamn: " + LastName} \n{"E-post: " + Email}\n";
-
     string DisplayAllInfo => $"\n{"Förnamn: " + FirstName} \n{"Efternamn: " + LastName} \n{"E-post: " + Email}\n{"Telefonnummer: " + Phone} \n{"Adress: " + Address}{","} {City}\n";
 }
 
